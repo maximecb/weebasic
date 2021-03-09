808d0635ead9b307a8580faf523f682e71f07dab
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-03-08 22:19:33 -0500

    Update weebasic.c

--- weebasic.c
+++ weebasic.c
@@ -1,1 +1,19 @@
+typedef enum
+{
+    OP_PUSH_IMM;
 
+} opcode_t;
+
+typedef struct
+{
+    opcode_t op;
+  
+    union imm
+    {
+    }
+} instr_t;
+      
+int main(int argc, char** argv)
+{
+    return 0;
+}
